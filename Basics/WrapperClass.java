8527c05131b44ae15c739563b1cecca54b0f4a7d
Ashwani <xeoneditz1811@gmail.com>
2026-02-02 17:00:29 +0530

    Static variable.

---
+++ Basics/WrapperClass.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class WrapperClass {
+    public static void main(String[] args){
+        Integer a = 32;
+        int i = a;
+        double b = 43.552;
+        int c =(int) b;
+        System.out.println(c);
+        System.out.println(i);
+    }
+}
