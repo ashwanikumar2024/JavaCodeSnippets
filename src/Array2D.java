b6944f7212de03b2bd24a9360fb7372876018cdf
Ashwani <xeoneditz1811@gmail.com>
2026-02-11 10:22:56 +0530

    Array insertion

--- src/Array2D.java
+++ src/Array2D.java
@@ -4,16 +4,30 @@ public class Array2D {
         Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        int [][]arr = new int[n][n];
+// Pri-Define Array
+        //int [][]arr1 = new int[][]{{1,2,3}, {4,5,6}, {8,5,2}};
+//
+       //for(int i=0; i<arr1.length; i++){
+            //for(int j=0; j< arr1.length; j++){
+                //System.out.print(arr1[i][j] + " ");
+            //}System.out.println();
+       // }
+//        for(int []a  : arr1){
+//            for(int x: a){
+//                System.out.print(x + " ");
+//            }
+//        }
 
-        int [][]arr1 = new int[][]{{1,2,3}, {4,5,6}, {8,5,2}};
+// Array by user
+        int [][]arr = new int[3][];
 
-        for(int i=0; i<arr1.length; i++){
-            for(int j=0; j< arr1.length; j++){
-                System.out.print(arr1[i][j] + " ");
-            }System.out.println();
+        for(int i=0; i<arr.length; i++){
+            arr[i] = new int[sc.nextInt()];
+            for(int j = 0; j<arr[i].length; j++){
+                arr[i][j] = sc.nextInt();
+            }
         }
-
-        for(int []a  : arr1){
+        for(int []a  : arr){
             for(int x: a){
                 System.out.print(x + " ");
             }
